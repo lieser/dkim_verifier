4493dcdf527a27ef017a0b621862ba531098a5f8
lieser <lieser@server.fake>
2013-06-12 13:09:50 +0200

    updated German translation

--- chrome/locale/de-DE/dns.js
+++ chrome/locale/de-DE/dns.js
@@ -1,7 +1,7 @@
 var EXPORTED_SYMBOLS = ["DNS_STRINGS"];
 var DNS_STRINGS = new Object();
-DNS_STRINGS.TOO_MANY_HOPS = "Zu viele Server-SprÃ¼nge.";
+DNS_STRINGS.TOO_MANY_HOPS = "Zu viele Server-Sprünge.";
 DNS_STRINGS.CONNECTION_REFUSED = function(server) { return "DNS Server " + server + " verweigert eine TCP Verbindung."; };
-DNS_STRINGS.TIMED_OUT = function(server) { return "DNS Server " + server + " hat Zeitlimit fÃ¼r eine TCP Verbindung Ã¼berschritten."; };
+DNS_STRINGS.TIMED_OUT = function(server) { return "DNS Server " + server + " hat Zeitlimit für eine TCP Verbindung überschritten."; };
 DNS_STRINGS.SERVER_ERROR = function(server) { return "Fehler bei der Verbindung zum DNS-Server " + server + "."; };
-DNS_STRINGS.INCOMPLETE_RESPONSE = function(server) { return "UnvollstÃ¤ndige Antwort von " + server + "."; };
+DNS_STRINGS.INCOMPLETE_RESPONSE = function(server) { return "Unvollständige Antwort von " + server + "."; };
