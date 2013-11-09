cc9fbfc4e0799719b3d76635c666a098f249255c
lieser <lieser@server.fake>
2013-11-09 18:57:12 +0100

    added libunbound preferences to add-on options

--- chrome/content/options_general.js
+++ chrome/content/options_general.js
@@ -1,7 +1,8 @@
 var gDKIMOptionsGeneralPane = {
 	init: function () {
-		this.update_Policy_signRules_enable();
 		this.update_key_storing();
+		this.update_dns_resolver();
+		this.update_Policy_signRules_enable();
 	},
 	
 	update_key_storing: function () {
@@ -9,6 +10,11 @@ var gDKIMOptionsGeneralPane = {
 		document.getElementById("key.viewKeys").disabled = disabled;
 	},
 	
+	update_dns_resolver: function () {
+		var deckIndex = document.getElementById("dns.resolver.label").value - 1;
+		document.getElementById("resolverDeck").selectedIndex = deckIndex;
+	},
+
 	update_Policy_signRules_enable: function () {
 		var disabled = !document.getElementById("policy.signRules.enable").checked;
 		document.getElementById("policy.signRules.checkDefaultRules").disabled = disabled;
