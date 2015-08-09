c3cbf43e9e7a7e88ec28cbd076fb0f8f06e35ae0
lieser <lieser@users.noreply.github.com>
2015-08-09 14:55:58 +0200

    Test/SPF.jsm/IP: added additional tests

--- tests/SPF-test-IP.js
+++ tests/SPF-test-IP.js
@@ -24,13 +24,26 @@ function ipv6_parse() {
 		(new SPF._IP("2001:db8:85a3:0:0:8a2e:370:7334"))._buffer,
 		[32,1,13,184,133,163,0,0,0,0,138,46,3,112,115,52],
 		"ipv6_parse leading zeroes"
-
 	);
 	Assert.deepEqual(
 		(new SPF._IP("2001:db8:85a3::8a2e:370:7334"))._buffer,
 		[32,1,13,184,133,163,0,0,0,0,138,46,3,112,115,52],
-		"ipv6_parse groups of zeroes"
-
+		"ipv6_parse groups of zeroes 01"
+	);
+	Assert.deepEqual(
+		(new SPF._IP("::0db8:85a3:0000:0000:8a2e:0370:7334"))._buffer,
+		[0,0,13,184,133,163,0,0,0,0,138,46,3,112,115,52],
+		"ipv6_parse groups of zeroes 02"
+	);
+	Assert.deepEqual(
+		(new SPF._IP("2001:0db8:85a3:0000:0000:8a2e:0370::"))._buffer,
+		[32,1,13,184,133,163,0,0,0,0,138,46,3,112,0,0],
+		"ipv6_parse groups of zeroes 03"
+	);
+	Assert.deepEqual(
+		(new SPF._IP("::"))._buffer,
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		"ipv6_parse all zero"
 	);
 }
 
