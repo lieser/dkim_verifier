356e153823c148d905e8957df29ae8e59c874704
lieser <lieser@users.noreply.github.com>
2019-02-17 17:03:58 +0100

    fix options GUI

--- chrome/content/options_general.js
+++ chrome/content/options_general.js
@@ -1,6 +1,12 @@
+/* eslint-env browser */
+/* eslint strict: ["warn", "function"] */
+/* exported gDKIMOptionsGeneralPane */
+// @ts-nocheck
+
 var gDKIMOptionsGeneralPane = {
 	init: function () {
-		gDKIMonpaneload("paneGeneral");
+		"use strict";
+
 		this.update_key_storing();
 		this.update_dns_resolver();
 		this.update_dns_proxy();
@@ -8,24 +14,32 @@ var gDKIMOptionsGeneralPane = {
 	},
 	
 	update_key_storing: function () {
+		"use strict";
+
 		var disabled = document.getElementById("key.storing").value == 0;
 		document.getElementById("key.viewKeys").disabled = disabled;
 	},
 	
 	update_dns_resolver: function () {
+		"use strict";
+
 		var deckIndex = document.getElementById("dns.resolver.label").value - 1;
 		document.getElementById("resolverDeck").selectedIndex = deckIndex;
 	},
 
 	update_dns_proxy: function () {
+		"use strict";
+
 		var disabled = !document.getElementById("dns.proxy.enable").checked;
-		var proxyConfigs = document.querySelectorAll(".proxyConfig")
+		var proxyConfigs = document.querySelectorAll(".proxyConfig");
 		for (var e of proxyConfigs) {
 			e.disabled = disabled;
 		}
 	},
 
 	update_Policy_signRules_enable: function () {
+		"use strict";
+
 		var disabled = !document.getElementById("policy.signRules.enable").checked;
 		document.getElementById("policy.signRules.checkDefaultRules").disabled = disabled;
 		document.getElementById("policy.signRules.autoAddRule").disabled = disabled;
@@ -39,6 +53,8 @@ var gDKIMOptionsGeneralPane = {
 	},
 	
 	update_Policy_autoAddRule_enable: function () {
+		"use strict";
+
 		var disabled = !document.getElementById("policy.signRules.enable").checked ||
 			!document.getElementById("policy.signRules.autoAddRule").checked;
 		document.getElementById("policy.signRules.autoAddRule.onlyIfFromAddressInSDID").
