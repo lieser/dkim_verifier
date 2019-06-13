a8c70f75e185273c1298c42d755f8ee62f39fb91
lieser <lieser@users.noreply.github.com>
2019-06-13 23:32:12 +0200

    fix GUI for account specific options

--- components/accountManagerExtension.js
+++ components/accountManagerExtension.js
@@ -13,10 +13,13 @@
  * included in all copies or substantial portions of the Software.
  */
 
+/* eslint strict: ["warn", "function"] */
+/* global Components, ChromeUtils, XPCOMUtils */
+
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
 //class constructor
-function DkimVerifierAccountManagerExtension() {}
+function DkimVerifierAccountManagerExtension() {"use strict";}
 
 // class definition
 DkimVerifierAccountManagerExtension.prototype = {
@@ -25,17 +28,14 @@ DkimVerifierAccountManagerExtension.prototype = {
 	classID: Components.ID("{850258f2-ae8b-421a-8a9c-bd99fdcc8097}"),
 	classDescription: "DKIM Verifier Account Manager Extension",
 	contractID: "@mozilla.org/accountmanager/extension;1?name=dkim_verifier-prefs",
-	_xpcom_categories: [{
-		category: "mailnews-accountmanager-extensions",
-		entry: "DKIM Verifier Account Manager Extension",
-		service: false
-	}],
 
-	QueryInterface: XPCOMUtils.generateQI([
+	QueryInterface: ChromeUtils.generateQI([
 		Components.interfaces.nsIMsgAccountManagerExtension
 	]),
 	showPanel: function(server) 
 	{
+		"use strict";
+
 		// this panel is only shown for imap and pop3 accounts
 		if (server.type === "imap" || server.type === "pop3") {
 			return true;
@@ -44,5 +44,6 @@ DkimVerifierAccountManagerExtension.prototype = {
 	},
 };
 
+// create factory for account setting extension
 var components = [DkimVerifierAccountManagerExtension];
 const NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
