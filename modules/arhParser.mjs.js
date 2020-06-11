e1134200365eeffa20b14e10a0c173324b719e3f
lieser <lieser@users.noreply.github.com>
2020-06-11 21:43:40 +0200

    convert ARH parser to esm module, and fix parsing of version

--- modules/arhParser.mjs.js
+++ modules/arhParser.mjs.js
@@ -1,156 +1,127 @@
 /*
- * ARHParser.jsm
- * 
  * Parser for the Authentication-Results header as specified in RFC 7601.
  *
- * Version: 1.2.1 (13 January 2019)
- * 
- * Copyright (c) 2014-2019 Philippe Lieser
- * 
+ * Copyright (c) 2014-2020 Philippe Lieser
+ *
  * This software is licensed under the terms of the MIT License.
- * 
+ *
  * The above copyright and license notice shall be
  * included in all copies or substantial portions of the Software.
  */
 
-// options for ESLint
-/* global Components, Services */
-/* global Logging */
-/* exported EXPORTED_SYMBOLS, ARHParser */
+// @ts-check
+/* eslint-disable camelcase */
+/* eslint-disable no-use-before-define */
 
-"use strict";
+import Logging from "./logging.mjs.js";
+import RfcParser from "./rfcParser.mjs.js";
 
-// @ts-ignore
-const module_version = "1.2.1";
-
-var EXPORTED_SYMBOLS = [
-	"ARHParser"
-];
-
-// @ts-ignore
-const PREF_BRANCH = "extensions.dkim_verifier.arh.";
-
-// @ts-ignore
-const Cu = Components.utils;
-
-Cu.import("resource://gre/modules/Services.jsm");
-
-Cu.import("resource://dkim_verifier/logging.jsm");
-
-
-// @ts-ignore
-var prefs = Services.prefs.getBranch(PREF_BRANCH);
-// @ts-ignore
-let log = Logging.getLogger("ARHParser");
+const log = Logging.getLogger("ArhParser");
 
 
 // WSP as specified in Appendix B.1 of RFC 5234
-let WSP_p = "[ \t]";
+const WSP_p = "[ \t]";
 // VCHAR as specified in Appendix B.1 of RFC 5234
-let VCHAR_p = "[!-~]";
+const VCHAR_p = "[!-~]";
 // Let-dig  as specified in Section 4.1.2 of RFC 5321 [SMTP].
-let Let_dig_p = "[A-Za-z0-9]";
+const Let_dig_p = "[A-Za-z0-9]";
 // Ldh-str  as specified in Section 4.1.2 of RFC 5321 [SMTP].
-let Ldh_str_p = "(?:[A-Za-z0-9-]*" + Let_dig_p + ")";
+const Ldh_str_p = `(?:[A-Za-z0-9-]*${Let_dig_p})`;
 // "Keyword" as specified in Section 4.1.2 of RFC 5321 [SMTP].
-let Keyword_p = Ldh_str_p;
-// sub-domain as specified in Section 4.1.2 of RFC 5321 [SMTP].
-let sub_domain_p = "(?:" + Let_dig_p + Ldh_str_p + "?)";
+const Keyword_p = Ldh_str_p;
 // obs-FWS as specified in Section 4.2 of RFC 5322
-let obs_FWS_p = "(?:" + WSP_p + "+(?:\r\n" + WSP_p + "+)*)";
+const obs_FWS_p = `(?:${WSP_p}+(?:\r\n${WSP_p}+)*)`;
 // quoted-pair as specified in Section 3.2.1 of RFC 5322
 // Note: obs-qp is not included, so this pattern matches less then specified!
-let quoted_pair_p = "(?:\\\\(?:" + VCHAR_p + "|" + WSP_p + "))";
+const quoted_pair_p = `(?:\\\\(?:${VCHAR_p}|${WSP_p}))`;
 // FWS as specified in Section 3.2.2 of RFC 5322
-let FWS_p = "(?:(?:(?:" + WSP_p + "*\r\n)?" + WSP_p + "+)|" + obs_FWS_p + ")";
-let FWS_op = FWS_p + "?";
+const FWS_p = `(?:(?:(?:${WSP_p}*\r\n)?${WSP_p}+)|${obs_FWS_p})`;
+const FWS_op = `${FWS_p}?`;
 // ctext as specified in Section 3.2.2 of RFC 5322
-let ctext_p = "[!-'*-[\\]-~]";
+const ctext_p = "[!-'*-[\\]-~]";
 // ccontent as specified in Section 3.2.2 of RFC 5322
 // Note: comment is not included, so this pattern matches less then specified!
-let ccontent_p = "(?:" + ctext_p + "|" + quoted_pair_p + ")";
+const ccontent_p = `(?:${ctext_p}|${quoted_pair_p})`;
 // comment as specified in Section 3.2.2 of RFC 5322
-let comment_p = "\\((?:" + FWS_op + ccontent_p + ")*" + FWS_op + "\\)";
+const comment_p = `\\((?:${FWS_op}${ccontent_p})*${FWS_op}\\)`;
 // CFWS as specified in Section 3.2.2 of RFC 5322 [MAIL]
-let CFWS_p = "(?:(?:(?:" + FWS_op + comment_p + ")+" + FWS_op + ")|" + FWS_p + ")";
-let CFWS_op = CFWS_p + "?";
-// atext as specified in Section 3.2.3 of RFC 5322
-let atext_p = "[!#-'*-+/-9=?A-Z^-~-]";
+const CFWS_p = `(?:(?:(?:${FWS_op}${comment_p})+${FWS_op})|${FWS_p})`;
+const CFWS_op = `${CFWS_p}?`;
 // dot-atom-text as specified in Section 3.2.3 of RFC 5322
-let dot_atom_text_p = "(?:" + atext_p + "+(?:\\." + atext_p + "+)*)";
+const dot_atom_text_p = RfcParser.dot_atom_text;
 // dot-atom as specified in Section 3.2.3 of RFC 5322
 // dot-atom        =   [CFWS] dot-atom-text [CFWS]
-let dot_atom_p = "(?:" + CFWS_op + dot_atom_text_p + CFWS_op + ")";
+const dot_atom_p = `(?:${CFWS_op}${dot_atom_text_p}${CFWS_op})`;
 // qtext as specified in Section 3.2.4 of RFC 5322
 // Note: obs-qtext is not included, so this pattern matches less then specified!
-let qtext_p = "[!#-[\\]-~]";
+const qtext_p = "[!#-[\\]-~]";
 // qcontent as specified in Section 3.2.4 of RFC 5322
-let qcontent_p = "(?:" + qtext_p + "|" + quoted_pair_p + ")";
+const qcontent_p = `(?:${qtext_p}|${quoted_pair_p})`;
 // quoted-string as specified in Section 3.2.4 of RFC 5322
-let quoted_string_p = "(?:" + CFWS_op +
-	"\"(?:" + FWS_op + qcontent_p + ")*" + FWS_op + "\"" +
-	CFWS_op + ")";
-let quoted_string_cp = "(?:" + CFWS_op +
-	"\"((?:" + FWS_op + qcontent_p + ")*)" + FWS_op + "\"" +
-	CFWS_op + ")";
+const quoted_string_p = `(?:${CFWS_op}"(?:${FWS_op}${qcontent_p})*${FWS_op}"${CFWS_op})`;
+const quoted_string_cp = `(?:${CFWS_op}"((?:${FWS_op}${qcontent_p})*)${FWS_op}"${CFWS_op})`;
 // local-part as specified in Section 3.4.1 of RFC 5322
 // Note: obs-local-part is not included, so this pattern matches less then specified!
-let local_part_p = "(?:" + dot_atom_p + "|" + quoted_string_p + ")";
+const local_part_p = `(?:${dot_atom_p}|${quoted_string_p})`;
 // token as specified in Section 5.1 of RFC 2045.
-let token_p = "[^ \\x00-\\x1F\\x7F()<>@,;:\\\\\"/[\\]?=]+";
+const token_p = "[^ \\x00-\\x1F\\x7F()<>@,;:\\\\\"/[\\]?=]+";
 // "value" as specified in Section 5.1 of RFC 2045.
-let value_p = "(?:" + token_p + "|" + quoted_string_p + ")";
-let value_cp = "(?:(" + token_p + ")|" + quoted_string_cp + ")";
+const value_p = `(?:${token_p}|${quoted_string_p})`;
+const value_cp = `(?:(${token_p})|${quoted_string_cp})`;
 // domain-name as specified in Section 3.5 of RFC 6376 [DKIM].
-let domain_name_p = "(?:" + sub_domain_p + "(?:\\." + sub_domain_p + ")+)";
+const domain_name_p = RfcParser.domain_name;
 
 
 /**
- * @typedef {Object} ARHHeader
- * @property {String} authserv_id
- * @property {Number} [authres_version]
- * @property {ARHResinfo[]} resinfo
+ * @typedef {Object} ArhHeader
+ * @property {string} authserv_id
+ * @property {number} authres_version
+ * @property {ArhResInfo[]} resinfo
  */
 
 /**
- * @typedef {Object} ARHResinfo
- * @property {String} method
- * @property {Number} [method_version]
- * @property {String} result
+ * @typedef {Object.<string, string|undefined>} ArhProperty
+ */
+/**
+ * @typedef {{[x: string]: ArhProperty|undefined, smtp: ArhProperty, header: ArhProperty, body: ArhProperty, policy: ArhProperty }} ArhProperties
+ */
+
+/**
+ * @typedef {Object} ArhResInfo
+ * @property {string} method
+ * @property {number} method_version
+ * @property {string} result
  *           none|pass|fail|softfail|policy|neutral|temperror|permerror
- * @property {String} [reason]
- * @property {Object} propertys
- * @property {Object} propertys.smtp
- * @property {Object} propertys.header
- * @property {Object} propertys.body
- * @property {Object} propertys.policy
- * @property {Object} [propertys._Keyword_]
- *           ARHResinfo can also include other propertys besides the aboves.
+ * @property {string} [reason]
+ * @property {ArhProperties} propertys
+ * @property {ArhProperty} propertys.smtp
+ * @property {ArhProperty} propertys.header
+ * @property {ArhProperty} propertys.body
+ * @property {ArhProperty} propertys.policy
+ * @property {ArhProperty} [propertys._Keyword_]
+ *           ArhResInfo can also include other propertys besides the aboves.
  */
 
-let ARHParser = {
-	get version() { return module_version; },
-
+export default class ArhParser {
 	/**
 	 *  Parses an Authentication-Results header.
-	 *  
-	 *  @param {String} authresHeader Authentication-Results header
-	 *  @return {ARHHeader} Parsed Authentication-Results header
+	 *
+	 *  @param {string} authResHeader Authentication-Results header
+	 *  @param {boolean} [relaxedParsing] Enable relaxed parsing
+	 *  @return {ArhHeader} Parsed Authentication-Results header
 	 */
-	parse: function _ARHParser_parse(authresHeader) {
+	static parse(authResHeader, relaxedParsing = false) {
 		// remove header name
-		authresHeader = authresHeader.replace(
-			new RegExp("^Authentication-Results:"+CFWS_op, "i"), "");
-		let authresHeaderRef = new RefString(authresHeader);
+		const authResHeaderRef = new RefString(authResHeader.replace(
+			new RegExp(`^Authentication-Results:${CFWS_op}`, "i"), ""));
 
-		/** @type {ARHHeader} */
-		let res = {};
+		/** @type {ArhHeader} */
+		const res = {};
 		res.resinfo = [];
 		let reg_match;
 
 		// get authserv-id and authres-version
-		reg_match = match(authresHeaderRef,
-			value_cp + "(?:" + CFWS_p + "([0-9]+)" + CFWS_op +" )?");
+		reg_match = match(authResHeaderRef, `${value_cp}(?:${CFWS_p}([0-9]+)${CFWS_op})?`);
 		res.authserv_id = reg_match[1] || reg_match[2];
 		if (reg_match[3]) {
 			res.authres_version = parseInt(reg_match[3], 10);
@@ -159,51 +130,50 @@ let ARHParser = {
 		}
 
 		// check if message authentication was performed
-		reg_match = match_o(authresHeaderRef, ";" + CFWS_op+"?none");
+		reg_match = match_o(authResHeaderRef, `;${CFWS_op}?none`);
 		if (reg_match !== null) {
 			log.debug("no-result");
 			return res;
 		}
 
 		// get the resinfos
-		while (authresHeaderRef.value !== "") {
-			let arhResInfo = parseResinfo(authresHeaderRef);
+		while (authResHeaderRef.value !== "") {
+			const arhResInfo = parseResInfo(authResHeaderRef, relaxedParsing);
 			if (arhResInfo) {
 				res.resinfo.push(arhResInfo);
 			}
 		}
 
-		log.debug(res.toSource());
 		return res;
-	},
-};
+	}
+}
 
 /**
  *  Parses the next resinfo in str. The parsed part of str is removed from str.
- *  
+ *
  *  @param {RefString} str
- *  @return {ARHResinfo|null} Parsed resinfo
+ *  @param {boolean} relaxedParsing Enable relaxed parsing
+ *  @return {ArhResInfo|null} Parsed resinfo
  */
-function parseResinfo(str) {
-	log.trace("parse str: " + str.toSource());
+function parseResInfo(str, relaxedParsing) {
+	log.trace("parse str: ", str);
 
 	let reg_match;
-	/** @type {ARHResinfo} */
-	let res = {};
-	
+	/** @type {ArhResInfo} */
+	const res = {};
+
 	// get methodspec
-	let method_version_p = CFWS_op + "/" + CFWS_op + "([0-9]+)";
-	let method_p = "(" + Keyword_p + ")(?:" + method_version_p + ")?";
+	const method_version_p = `${CFWS_op}/${CFWS_op}([0-9]+)`;
+	const method_p = `(${Keyword_p})(?:${method_version_p})?`;
 	let Keyword_result_p = "none|pass|fail|softfail|policy|neutral|temperror|permerror";
 	// older SPF specs (e.g. RFC 4408) use mixed case
 	Keyword_result_p += "|None|Pass|Fail|SoftFail|Neutral|TempError|PermError";
-	let result_p = "=" + CFWS_op + "(" + Keyword_result_p + ")";
-	let methodspec_p = ";" + CFWS_op + method_p + CFWS_op + result_p;
+	const result_p = `=${CFWS_op}(${Keyword_result_p})`;
+	const methodspec_p = `;${CFWS_op}${method_p}${CFWS_op}${result_p}`;
 	try {
 		reg_match = match(str, methodspec_p);
 	} catch (exception) {
-		if (prefs.getBoolPref("relaxedParsing"))
-		{
+		if (relaxedParsing) {
 			// allow trailing ";" at the end
 			match_o(str, ";");
 			if (str.value.trim() === "") {
@@ -222,35 +192,36 @@ function parseResinfo(str) {
 	res.result = reg_match[3].toLowerCase();
 
 	// get reasonspec (optional)
-	let reasonspec_p = "reason" + CFWS_op + "=" + CFWS_op + value_cp;
+	const reasonspec_p = `reason${CFWS_op}=${CFWS_op}${value_cp}`;
 	reg_match = match_o(str, reasonspec_p);
 	if (reg_match !== null) {
 		res.reason = reg_match[1] || reg_match[2];
 	}
 
 	// get propspec (optional)
-	let pvalue_p = value_p + "|(?:(?:" + local_part_p + "?@)?" + domain_name_p + ")";
-	if (prefs.getBoolPref("relaxedParsing")) {
+	let pvalue_p = `${value_p}|(?:(?:${local_part_p}?@)?${domain_name_p})`;
+	if (relaxedParsing) {
 		// allow "/" in the header.b (or other) property, even if it is not in a quoted-string
 		pvalue_p += "|[^ \\x00-\\x1F\\x7F()<>@,;:\\\\\"[\\]?=]+";
 	}
-	let special_smtp_verb_p = "mailfrom|rcptto";
-	let property_p = special_smtp_verb_p + "|" + Keyword_p;
-	let propspec_p = "(" + Keyword_p + ")" + CFWS_op + "\\." + CFWS_op +
-		"(" + property_p + ")" + CFWS_op + "=" + CFWS_op + "(" + pvalue_p + ")";
+	const special_smtp_verb_p = "mailfrom|rcptto";
+	const property_p = `${special_smtp_verb_p}|${Keyword_p}`;
+	const propspec_p = `(${Keyword_p})${CFWS_op}\\.${CFWS_op}(${property_p})${CFWS_op}=${CFWS_op}(${pvalue_p})`;
 	res.propertys = {};
 	res.propertys.smtp = {};
 	res.propertys.header = {};
 	res.propertys.body = {};
 	res.propertys.policy = {};
 	while ((reg_match = match_o(str, propspec_p)) !== null) {
-		if (!res.propertys[reg_match[1]]) {
-			res.propertys[reg_match[1]] = {};
+		let property = res.propertys[reg_match[1]];
+		if (!property) {
+			property = {};
+			res.propertys[reg_match[1]] = property;
 		}
-		res.propertys[reg_match[1]][reg_match[2]] = reg_match[3];
+		property[reg_match[2]] = reg_match[3];
 	}
 
-	log.trace(res.toSource());
+	log.trace("parseResInfo res:", res);
 	return res;
 }
 
@@ -266,14 +237,20 @@ class RefString {
 	constructor(s) {
 		this.value = s;
 	}
-	match() {
-		return this.value.match.apply(this.value, arguments);
+	/**
+	 * @param {RegExp} regexp
+	 * @returns {RegExpMatchArray?}
+	 */
+	match(regexp) {
+		return this.value.match(regexp);
 	}
-	substr() {
-		return this.value.substr.apply(this.value, arguments);
-	}
-	toSource() {
-		return this.value.toSource.apply(this.value, arguments);
+	/**
+	 * @param {number} from
+	 * @param {number} [length]
+	 * @returns {string}
+	 */
+	substr(from, length) {
+		return this.value.substr(from, length);
 	}
 }
 
@@ -282,16 +259,16 @@ class RefString {
  *  Adds CFWS_op to the beginning of pattern.
  *  pattern must be followed by string end, ";" or CFWS_p.
  *  Removes the found match from str.
- *  
+ *
  *  @param {RefString} str
- *  @param {String} pattern
- *  @return {String[]} An Array, containing the matches
+ *  @param {string} pattern
+ *  @return {string[]} An Array, containing the matches
  *  @throws if match no match found
  */
 function match(str, pattern) {
-	let reg_match = match_o(str, pattern);
+	const reg_match = match_o(str, pattern);
 	if (reg_match === null) {
-		log.trace("str to match against:" + str.toSource());
+		log.trace("str to match against:", JSON.stringify(str));
 		throw new Error("Parsing error");
 	}
 	return reg_match;
@@ -302,20 +279,20 @@ function match(str, pattern) {
  *  Adds CFWS_op to the beginning of pattern.
  *  pattern must be followed by string end, ";" or CFWS_p.
  *  If match is found, removes it from str.
- *  
+ *
  *  @param {RefString} str
- *  @param {String} pattern
- *  @return {String[]|Null} Null if no match for the pattern is found, else
+ *  @param {string} pattern
+ *  @return {string[]|Null} Null if no match for the pattern is found, else
  *                        an Array, containing the matches
  */
 function match_o(str, pattern) {
-	let regexp = new RegExp("^" + CFWS_op + "(?:" + pattern + ")" +
-		"(?:(?:" + CFWS_op + "\r\n$)|(?=;)|(?=" + CFWS_p + "))");
-	let reg_match = str.match(regexp);
+	const regexp = new RegExp(`^${CFWS_op}(?:${pattern})` +
+		`(?:(?:${CFWS_op}\r\n$)|(?=;)|(?=${CFWS_p}))`);
+	const reg_match = str.match(regexp);
 	if (reg_match === null) {
 		return null;
 	}
-	log.trace("matched: " + reg_match[0].toSource());
+	log.trace("matched: ", JSON.stringify(reg_match[0]));
 	str.value = str.substr(reg_match[0].length);
 	return reg_match;
 }
---
+++ test/unittest/arhParserSpec.mjs.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2020 Philippe Lieser
+ *
+ * This software is licensed under the terms of the MIT License.
+ *
+ * The above copyright and license notice shall be
+ * included in all copies or substantial portions of the Software.
+ */
+
+// @ts-check
+
+import ArhParser from "../../modules/arhParser.mjs.js";
+import expect from "../helpers/chaiUtils.mjs.js";
+
+describe("ARH Parser [unittest]", function () {
+	describe("RFC 7601 Appendix B Example", function () {
+		it("B.2.  Nearly Trivial Case; Service Provided, but No Authentication Done", async function () {
+			const res = await ArhParser.parse(
+				"Authentication-Results: example.org 1; none\r\n");
+			expect(res.authserv_id).to.be.equal("example.org");
+			expect(res.authres_version).to.be.equal(1);
+			expect(res.resinfo.length).to.be.equal(0);
+		});
+		it("B.3.  Service Provided, Authentication Done", async function () {
+			const res = await ArhParser.parse(
+				"Authentication-Results: example.com;\r\n" +
+				"          spf=pass smtp.mailfrom=example.net\r\n");
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.authres_version).to.be.equal(1);
+			expect(res.resinfo.length).to.be.equal(1);
+			expect(res.resinfo[0].method).to.be.equal("spf");
+			expect(res.resinfo[0].method_version).to.be.equal(1);
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].propertys.smtp.mailfrom).to.be.equal("example.net");
+		});
+		it("B.4.  Service Provided, Several Authentications Done, Single MTA", async function () {
+			let res = await ArhParser.parse(
+				"Authentication-Results: example.com;\r\n" +
+				"          auth=pass (cram-md5) smtp.auth=sender@example.net;\r\n" +
+				"          spf=pass smtp.mailfrom=example.net\r\n");
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.resinfo.length).to.be.equal(2);
+			expect(res.resinfo[0].method).to.be.equal("auth");
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].propertys.smtp.auth).to.be.equal("sender@example.net");
+			expect(res.resinfo[1].method).to.be.equal("spf");
+			expect(res.resinfo[1].result).to.be.equal("pass");
+			expect(res.resinfo[1].propertys.smtp.mailfrom).to.be.equal("example.net");
+			res = await ArhParser.parse(
+				"Authentication-Results: example.com;\r\n" +
+				"          sender-id=pass header.from=example.net\r\n");
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.resinfo.length).to.be.equal(1);
+			expect(res.resinfo[0].method).to.be.equal("sender-id");
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].propertys.header.from).to.be.equal("example.net");
+		});
+		it("B.5.  Service Provided, Several Authentications Done, Different MTAs", async function () {
+			let res = await ArhParser.parse(
+				"Authentication-Results: example.com;\r\n" +
+				"          sender-id=fail header.from=example.com;\r\n" +
+				"          dkim=pass (good signature) header.d=example.com\r\n");
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.resinfo.length).to.be.equal(2);
+			expect(res.resinfo[0].method).to.be.equal("sender-id");
+			expect(res.resinfo[0].result).to.be.equal("fail");
+			expect(res.resinfo[0].propertys.header.from).to.be.equal("example.com");
+			expect(res.resinfo[1].method).to.be.equal("dkim");
+			expect(res.resinfo[1].result).to.be.equal("pass");
+			expect(res.resinfo[1].propertys.header.d).to.be.equal("example.com");
+			res = await ArhParser.parse(
+				"Authentication-Results: example.com;\r\n" +
+				"          auth=pass (cram-md5) smtp.auth=sender@example.com;\r\n" +
+				"          spf=fail smtp.mailfrom=example.com\r\n");
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.resinfo.length).to.be.equal(2);
+			expect(res.resinfo[0].method).to.be.equal("auth");
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].propertys.smtp.auth).to.be.equal("sender@example.com");
+			expect(res.resinfo[1].method).to.be.equal("spf");
+			expect(res.resinfo[1].result).to.be.equal("fail");
+			expect(res.resinfo[1].propertys.smtp.mailfrom).to.be.equal("example.com");
+		});
+		it("B.6.  Service Provided, Multi-tiered Authentication Done", async function () {
+			let res = await ArhParser.parse(
+				'Authentication-Results: example.com;\r\n' +
+				'      dkim=pass reason="good signature"\r\n' +
+				'        header.i=@mail-router.example.net;\r\n' +
+				'      dkim=fail reason="bad signature"\r\n' +
+				'        header.i=@newyork.example.com\r\n');
+			expect(res.authserv_id).to.be.equal("example.com");
+			expect(res.resinfo.length).to.be.equal(2);
+			expect(res.resinfo[0].method).to.be.equal("dkim");
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].reason).to.be.equal("good signature");
+			expect(res.resinfo[0].propertys.header.i).to.be.equal("@mail-router.example.net");
+			expect(res.resinfo[1].method).to.be.equal("dkim");
+			expect(res.resinfo[1].result).to.be.equal("fail");
+			expect(res.resinfo[1].reason).to.be.equal("bad signature");
+			expect(res.resinfo[1].propertys.header.i).to.be.equal("@newyork.example.com");
+			res = await ArhParser.parse(
+				"Authentication-Results: example.net;\r\n" +
+				"      dkim=pass (good signature) header.i=@newyork.example.com\r\n");
+			expect(res.authserv_id).to.be.equal("example.net");
+			expect(res.resinfo.length).to.be.equal(1);
+			expect(res.resinfo[0].method).to.be.equal("dkim");
+			expect(res.resinfo[0].result).to.be.equal("pass");
+			expect(res.resinfo[0].propertys.header.i).to.be.equal("@newyork.example.com");
+		});
+		it("B.7.  Comment-Heavy Example", async function () {
+			const res = await ArhParser.parse(
+				"Authentication-Results: foo.example.net (foobar) 1 (baz);\r\n" +
+				"    dkim (Because I like it) / 1 (One yay) = (wait for it) fail\r\n" +
+				"      policy (A dot can go here) . (like that) expired\r\n" +
+				"      (this surprised me) = (as I wasn't expecting it) 1362471462\r\n");
+			expect(res.authserv_id).to.be.equal("foo.example.net");
+			expect(res.authres_version).to.be.equal(1);
+			expect(res.resinfo.length).to.be.equal(1);
+			expect(res.resinfo[0].method).to.be.equal("dkim");
+			expect(res.resinfo[0].method_version).to.be.equal(1);
+			expect(res.resinfo[0].result).to.be.equal("fail");
+			expect(res.resinfo[0].propertys.policy.expired).to.be.equal("1362471462");
+		});
+	});
+});
