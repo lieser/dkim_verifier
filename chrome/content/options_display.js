bdd8f2ea13657ddde8a99f1999ffb3f3b56aabd9
dodmi <4572946+dodmi@users.noreply.github.com>
2025-05-26 21:53:29 +0200

    Optional show key selector in details view

--- chrome/content/options_display.js
+++ chrome/content/options_display.js
@@ -17,6 +17,7 @@ let gDKIMOptionsDisplayPane = {
 		if (gDKIMOptionsDisplayPane.initDone) { return; }
 		gDKIMOptionsDisplayPane.checkBox.showAdvancedInfo = document.getElementById("display.advancedInfo.show");
 		gDKIMOptionsDisplayPane.checkBox.allSignatures = document.getElementById("display.advancedInfo.allSignatures");
+		gDKIMOptionsDisplayPane.checkBox.includeSelector = document.getElementById("display.advancedInfo.includeSelector");
 		gDKIMOptionsDisplayPane.checkBox.includeHeaders = document.getElementById("display.advancedInfo.includeHeaders");
 		gDKIMOptionsDisplayPane.toggleDetailsOptionsEnabled();
 		gDKIMOptionsDisplayPane.initDone = true;
@@ -26,6 +27,7 @@ let gDKIMOptionsDisplayPane = {
 		"use strict";
 		let disabled = !gDKIMOptionsDisplayPane.checkBox.showAdvancedInfo.checked;
 		gDKIMOptionsDisplayPane.checkBox.allSignatures.disabled = disabled;
+		gDKIMOptionsDisplayPane.checkBox.includeSelector.disabled = disabled;
 		gDKIMOptionsDisplayPane.checkBox.includeHeaders.disabled = disabled;
 	}
 
