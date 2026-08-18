09f947025d97cc48e823c0f13d0cd9498d8ad8b0
dodmi <4572946+dodmi@users.noreply.github.com>
2026-08-18 22:13:41 +0200

    - Added logic for bimi options - Improved style for charamel/silvermel theme

--- chrome/content/options_display.js
+++ chrome/content/options_display.js
@@ -6,31 +6,59 @@
 let gDKIMOptionsDisplayPane = {
 	initDone : false,
 
-	checkBox : {
+	domElements : {
+		// Advanced info
 		showAdvancedInfo : null,
 		allSignatures : null,
-		includeHeaders : null
+		includeHeaders : null,
+		// FavIcons
+		showFavIcon : null,
+		preferFavIcon : null,
+		bimiEnable : null,
+		cacheBimi : null,
+		reloadBimi : null,
+		addBimiCa : null
 	},
 
 	init: function() {
 		"use strict";
 		if (gDKIMOptionsDisplayPane.initDone) { return; }
-		gDKIMOptionsDisplayPane.checkBox.showAdvancedInfo = document.getElementById("display.advancedInfo.show");
-		gDKIMOptionsDisplayPane.checkBox.allSignatures = document.getElementById("display.advancedInfo.allSignatures");
-		gDKIMOptionsDisplayPane.checkBox.includeSelector = document.getElementById("display.advancedInfo.includeSelector");
-		gDKIMOptionsDisplayPane.checkBox.includeHeaders = document.getElementById("display.advancedInfo.includeHeaders");
+		gDKIMOptionsDisplayPane.domElements.showAdvancedInfo = document.getElementById("display.advancedInfo.show");
+		gDKIMOptionsDisplayPane.domElements.allSignatures = document.getElementById("display.advancedInfo.allSignatures");
+		gDKIMOptionsDisplayPane.domElements.includeSelector = document.getElementById("display.advancedInfo.includeSelector");
+		gDKIMOptionsDisplayPane.domElements.includeHeaders = document.getElementById("display.advancedInfo.includeHeaders");
 		gDKIMOptionsDisplayPane.toggleDetailsOptionsEnabled();
+
+		gDKIMOptionsDisplayPane.domElements.showFavIcon = document.getElementById("display.icons.showFavIcon");
+		gDKIMOptionsDisplayPane.domElements.preferFavIcon = document.getElementById("display.icons.preferFavIcon");
+		gDKIMOptionsDisplayPane.domElements.bimiEnable = document.getElementById("display.icons.bimiEnable");
+		gDKIMOptionsDisplayPane.domElements.cacheBimi = document.getElementById("display.icons.cacheBimi");
+		gDKIMOptionsDisplayPane.domElements.reloadBimi = document.getElementById("display.icons.reloadBimi");
+		gDKIMOptionsDisplayPane.domElements.addBimiCa = document.getElementById("display.icons.addBimiCa");
+		gDKIMOptionsDisplayPane.toggleIconsOptionsEnabled();
+
 		gDKIMOptionsDisplayPane.initDone = true;
 	},
 
 	toggleDetailsOptionsEnabled: function() {
 		"use strict";
-		let disabled = !gDKIMOptionsDisplayPane.checkBox.showAdvancedInfo.checked;
-		gDKIMOptionsDisplayPane.checkBox.allSignatures.disabled = disabled;
-		gDKIMOptionsDisplayPane.checkBox.includeSelector.disabled = disabled;
-		gDKIMOptionsDisplayPane.checkBox.includeHeaders.disabled = disabled;
-	}
+		let disabled = !gDKIMOptionsDisplayPane.domElements.showAdvancedInfo.checked;
+		gDKIMOptionsDisplayPane.domElements.allSignatures.disabled = disabled;
+		gDKIMOptionsDisplayPane.domElements.includeSelector.disabled = disabled;
+		gDKIMOptionsDisplayPane.domElements.includeHeaders.disabled = disabled;
+	},
 
+	toggleIconsOptionsEnabled: function() {
+		"use strict";
+		let allDisabled = !gDKIMOptionsDisplayPane.domElements.showFavIcon.checked;
+		let bimiOnlineDisabled = gDKIMOptionsDisplayPane.domElements.bimiEnable.selectedIndex < 2;
+		let bimiCacheDisabled = !gDKIMOptionsDisplayPane.domElements.cacheBimi.checked;
+		gDKIMOptionsDisplayPane.domElements.preferFavIcon.disabled = allDisabled;
+		gDKIMOptionsDisplayPane.domElements.bimiEnable.disabled = allDisabled;
+		gDKIMOptionsDisplayPane.domElements.cacheBimi.disabled = allDisabled || bimiOnlineDisabled;
+		gDKIMOptionsDisplayPane.domElements.reloadBimi.disabled = allDisabled || bimiOnlineDisabled || bimiCacheDisabled;
+		gDKIMOptionsDisplayPane.domElements.addBimiCa.disabled = allDisabled || bimiOnlineDisabled;
+	}
 };
 
 window.addEventListener("paneload", gDKIMOptionsDisplayPane.init, false);
